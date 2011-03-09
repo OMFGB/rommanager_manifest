2b8a882557f88f21981df01e97494be291679371
r2doesinc <r2doesinc@t3hh4xx0r.(none)>
2011-03-09 01:40:07 -0600

    initial commit

---
+++ manifest.js
@@ -0,0 +1,84 @@
+
+{
+    version: 1,
+    donate: "http://forum.xda-developers.com/donatetome.php?u=3286081",
+    roms:
+    [
+	{
+        name: "OMFGB 1.1.0",
+        summary: "Android 2.3. Open Source Win",
+        device: "supersonic",
+        product: "OMGB",
+        incremental: 3,
+	url: "http://r2doesinc.bitsurge.net/OMFGB-1.1.0_evo.zip",
+	addons:
+	[
+	{
+		name: "Google Apps",
+		urls:
+		[
+		"http://www.r2doesinc.bitsurge.net/GAPPS.zip"
+		]
+	}	
+	]
+    	},
+    	
+	{
+        name: "OMFGB 1.1.0",
+        summary: "Android 2.3. Open Source Win",
+        device: "inc",
+        product: "OMGB",
+        incremental: 3,
+	url: "http://r2doesinc.bitsurge.net/OMFGB-1.1.0_inc.zip",
+	addons:
+	[
+	{
+		name: "Google Apps",
+		urls:
+		[
+		"http://www.r2doesinc.bitsurge.net/GAPPS.zip"
+		]
+	}
+	]
+	},	
+	
+	{
+        name: "OMFGB 1.1.1",
+        summary: "Android 2.3. Open Source Win",
+        device: "supersonic",
+        product: "OMGB",
+        incremental: 4,
+	url: "http://droid-themes.com/Downloads/OMFGB/OMFGB_supersonic-1.1.1.zip",
+	addons:
+	[
+	{
+		name: "Google Apps",
+		urls:
+		[
+		"http://www.r2doesinc.bitsurge.net/GAPPS.zip"
+		]
+	}
+	]
+	},
+	
+	{
+        name: "OMFGB 1.1.1",
+        summary: "Android 2.3. Open Source Win",
+        device: "inc",
+        product: "OMGB",
+        incremental: 4,
+	url: "http://droid-themes.com/Downloads/OMFGB/OMFGB_inc-1.1.1.zip",
+	addons:
+	[
+	{
+		name: "Google Apps",
+		urls:
+		[
+		"http://www.r2doesinc.bitsurge.net/GAPPS.zip"
+		]
+	}
+	]
+	}
+    ]
+}
+
