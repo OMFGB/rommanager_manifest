266699efa67c1a9bf30c0c5fa79bbde4e2f77055
jdkoreclipse <jdkoreclipse@gmail.com>
2011-03-10 17:40:45 -0800

    i lied, this is the last :(

--- manifest.js
+++ manifest.js
@@ -1,1 +1,95 @@
-{    version: 1,    donate: "http://forum.xda-developers.com/donatetome.php?u=3286081",    roms:    [	{        name: "OMFGB 1.1.0",        summary: "Custom Gingerbread build.",        device: "supersonic",        product: "OMFGB",        incremental: 3,	url: "http://r2doesinc.bitsurge.net/OMFGB-1.1.0_evo.zip",	addons:	[	{		name: "Google Apps",		urls:		[		"http://www.r2doesinc.bitsurge.net/GAPPS.zip"		]	}		]    	},    		{        name: "OMFGB 1.1.0",        summary: "Custom Gingerbread build.",        device: "inc",        product: "OMFGB",        incremental: 3,	url: "http://r2doesinc.bitsurge.net/OMFGB-1.1.0_inc.zip",	addons:	[	{		name: "Google Apps",		urls:		[		"http://www.r2doesinc.bitsurge.net/GAPPS.zip"		]	}	]	},		{        name: "OMFGB 1.1.1",        summary: "Custom Gingerbread build.",        device: "supersonic",        product: "OMFGB",        incremental: 4,	url: "http://droid-themes.com/Downloads/OMFGB/OMFGB_supersonic-1.1.1.zip",	addons:	[	{		name: "Google Apps",		urls:		[		"http://www.r2doesinc.bitsurge.net/GAPPS.zip"		]	}	]	},	{        name: "OMFGB 1.1.1",        summary: "Custom Gingerbread build.",        device: "inc",        product: "OMFGB",        incremental: 4,	url: "http://droid-themes.com/Downloads/OMFGB/OMFGB_inc-1.1.1.zip",	addons:	[	{		name: "Google Apps",		urls:		[		"http://www.r2doesinc.bitsurge.net/GAPPS.zip"		]	}	]	}    ]}
+{
+    version: 1,
+    donate: "http://forum.xda-developers.com/donatetome.php?u=3286081",
+    roms:
+    [
+	{
+        name: "OMFGB 1.1.0",
+        summary: "Custom Gingerbread build.",
+        device: "supersonic",
+        product: "OMFGB",
+        incremental: 3,
+	url: "http://r2doesinc.bitsurge.net/OMFGB-1.1.0_evo.zip",
+	addons:
+	[
+	{
+		name: "Google Apps",
+		urls:
+		[
+		"http://www.r2doesinc.bitsurge.net/GAPPS.zip"
+		]
+	}	
+	]
+    	},
+    	
+	{
+        name: "OMFGB 1.1.0",
+        summary: "Custom Gingerbread build.",
+        device: "inc",
+        product: "OMFGB",
+        incremental: 3,
+	url: "http://r2doesinc.bitsurge.net/OMFGB-1.1.0_inc.zip",
+	addons:
+	[
+	{
+		name: "Google Apps",
+		urls:
+		[
+		"http://www.r2doesinc.bitsurge.net/GAPPS.zip"
+		]
+	}
+	]
+	},	
+	
+	{
+        name: "OMFGB 1.1.1",
+        summary: "Custom Gingerbread build.",
+        device: "supersonic",
+        product: "OMFGB",
+        incremental: 4,
+	url: "http://droid-themes.com/Downloads/OMFGB/OMFGB_supersonic-1.1.1.zip",
+	addons:
+	[
+	{
+		name: "Google Apps",
+		urls:
+		[
+		"http://www.r2doesinc.bitsurge.net/GAPPS.zip"
+		]
+	}
+	]
+	},
+	
+	{
+        name: "OMFGB 1.1.1",
+        summary: "Custom Gingerbread build.",
+        device: "inc",
+        product: "OMFGB",
+        incremental: 4,
+	url: "http://droid-themes.com/Downloads/OMFGB/OMFGB_inc-1.1.1.zip",
+	addons:
+	[
+	{
+		name: "Google Apps",
+		urls:
+		[
+		"http://www.r2doesinc.bitsurge.net/GAPPS.zip"
+		]
+	}
+	]
+	},
+          {
+           name: "Jdkernel Gingerbread #4",
+           summary: "2.6.37 kernel by jdkoreclipse.",
+            device: "inc",
+           product: "OMFGB",
+           incremental: 4,
+url:"http://www.jdkoreclipse.com/files/jdkernel_4_inc.zip"
+                }
+	]
+	},
+
+
+    ]
+}
+
