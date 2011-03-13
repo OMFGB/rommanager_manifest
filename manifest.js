51a1443d91f05e5c13e9ec8f66fee358928e8e86
jdkoreclipse <jdkoreclipse@gmail.com>
2011-03-12 17:08:13 -0800

    Edited manifest.js via GitHub

--- manifest.js
+++ manifest.js
@@ -5,12 +5,6 @@
     [
 
 {
-    version: 1,
-    donate: "http://forum.xda-developers.com/donatetome.php?u=3286081",
-    roms:
-    [
-
-{
         name: "OMFGB 1.1.0",
         summary: "Custom Gingerbread build.",
         device: "supersonic",
@@ -144,144 +138,10 @@ name: "Jdkernel Froyo #1",
          url:"http://mirror.jdkoreclipse.net/kernel/jdkernel_froyo_inc.zip"
        }
        ]
-       },
-
-{
-        name: "OMFGB 1.1.0",
-        summary: "Custom Gingerbread build.",
-        device: "supersonic",
-        product: "OMFGB",
-        incremental: 3,
-	url: "http://r2doesinc.bitsurge.net/OMFGB-1.1.0_evo.zip",
-	addons:
-	[
-	{
-		name: "Google Apps",
-		urls:
-		[
-		"http://www.r2doesinc.bitsurge.net/GAPPS.zip"
-		]
-	}	
-	]
-    	},
-
-    	
-{
-        name: "OMFGB 1.1.0",
-        summary: "Custom Gingerbread build.",
-        device: "inc",
-        product: "OMFGB",
-        incremental: 3,
-	url: "http://r2doesinc.bitsurge.net/OMFGB-1.1.0_inc.zip",
-	addons:
-	[
-	{
-		name: "Google Apps",
-		urls:
-		[
-		"http://www.r2doesinc.bitsurge.net/GAPPS.zip"
-		]
-	}
-	]
-	},	
-
-{
-        name: "OMFGB 1.1.1",
-        summary: "Custom Gingerbread build.",
-        device: "supersonic",
-        product: "OMFGB",
-        incremental: 4,
-	url: "http://droid-themes.com/Downloads/OMFGB/OMFGB_supersonic-1.1.1.zip",
-	addons:
-	[
-	{
-		name: "Google Apps",
-		urls:
-		[
-		"http://www.r2doesinc.bitsurge.net/GAPPS.zip"
-		]
-	}
-	]
-	},
-
-	
-{
-        name: "OMFGB 1.1.1",
-        summary: "Custom Gingerbread build.",
-        device: "inc",
-        product: "OMFGB",
-        incremental: 4,
-	url: "http://droid-themes.com/Downloads/OMFGB/OMFGB_inc-1.1.1.zip",
-	addons:
-	[
-	{
-		name: "Google Apps",
-		urls:
-		[
-		"http://www.r2doesinc.bitsurge.net/GAPPS.zip"
-		]
-	}
-	]
-	},
-{
-        name: "Ninjablur for HTC Incredible",
-        summary: "For testing only!",
-        device: "inc",
-        product: "blurinc",
-        incremental: 4,
-	url: "http://mirror.jdkoreclipse.net/rom/blur.zip",
-	addons:
-	[
-	{
-		name: "Kernel (Mandatory d/l)",
-		urls:
-		[
-		"http://mirror.jdkoreclipse.net/rom/invisiblek.zip"
-		]
-	}	
-	]
-    	},
-
-{
-        name: "Android 2.4 Leak",
-        summary: "Leaked 2.4 build with slight mods.",
-        device: "inc",
-        product: "ginger",
-        incremental: 4,
-	url: "http://mirror.jdkoreclipse.net/rom/ginger2.4_inc_4.zip",
-	addons:
-	[
-	{
-		name: "Jdkernel #4",
-		urls:
-		[
-		"http://mirror.jdkoreclipse.net/kernel/jdkernel_4_inc.zip"
-		]
-	}	
-	]
-    	},
-{
-         name: "Jdkernel Gingerbread #4",
-         summary: "2.6.37 kernel by jdkoreclipse",
-         device: "inc",
-         product: "jdkernel",
-         incremental: 5,
-         url:"http://mirror.jdkoreclipse.net/kernel/jdkernel_4_inc.zip" 
-       }
-       ]
-       },
-
-{
-name: "Jdkernel Froyo #1",
-         summary: "2.6.37 Froyo kernel by jdkoreclipse",
-         device: "inc",
-         product: "jdkernel",
-         incremental: 4,
-         url:"http://mirror.jdkoreclipse.net/kernel/jdkernel_froyo_inc.zip" 
-       }
-       ]
        }
 
     ]
 }
+
+
 
