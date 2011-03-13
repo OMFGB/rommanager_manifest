5ae68d722f41af796889a55bee340d569e776308
jdkoreclipse <jdkoreclipse@gmail.com>
2011-03-12 17:11:45 -0800

    Edited manifest.js via GitHub

--- manifest.js
+++ manifest.js
@@ -124,12 +124,10 @@ urls:
          device: "inc",
          product: "jdkernel",
          incremental: 4,
-         url:"http://mirror.jdkoreclipse.net/kernel/jdkernel_4_inc.zip"
-       ]
+         url:"http://mirror.jdkoreclipse.net/kernel/jdkernel_4_inc.zip",
        }
        ]
-},
-
+       },
 {
 name: "Jdkernel Froyo #1",
          summary: "2.6.37 Froyo kernel by jdkoreclipse",
