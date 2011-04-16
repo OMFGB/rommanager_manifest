b041a02a4148e751f2a530d42ccbea4e87afecea
DrMacinyasha <drmacinyasha@gmail.com>
2011-04-15 18:32:28 -0700

    CLEAN

--- manifest.js
+++ manifest.js
@@ -1,178 +1,143 @@
 {
-    version: 1,
-    donate: "http://forum.xda-developers.com/donatetome.php?u=3286081",
-    roms:
+    "version": "1",
+    "homepage": "http://t3hh4xx0r.com/"
+    "donate": "http://forum.xda-developers.com/donatetome.php?u=3286081",
+    "roms": 
     [
-
-{
-        name: "OMGB 1.1.1",
-        summary: "Stock Gingerbread build.",
-        device: "desirec",
-        product: "OMGB",
-        incremental: 2,
-        url: "http://www.r2doesinc.bitsurge.net/OMGB_desirec-1.1.1.zip",
-        addons:
+    {
+        "name": "OMGB 1.1.1",
+        "summary": "Stock Gingerbread build.",
+        "device": "desirec",
+        "product": "OMGB",
+        "incremental": "2",
+        "url": "r2doesinc.bitsurge.net/OMGB_desirec-1.1.1.zip",
+        "addons": 
         [
-        {
-              name: "Google Apps",
-              urls:
-                   [
-                   "http://www.r2doesinc.bitsurge.net/GAPPS.zip"
-                   ]
-        }
+            {
+            "name": "Google Apps",
+            "url": "http://www.r2doesinc.bitsurge.net/GAPPS.zip"
+            }
         ]
-},
-
-{
-        name: "OMGB 1.1.1",
-        summary: "Stock Gingerbread build.",
-        device: "inc",
-        product: "OMGB",
-        incremental: 2,
-        url: "http://www.r2doesinc.bitsurge.net/OMGB_inc-1.1.1.zip",
-        addons:
+    },
+    {
+        "name": "OMGB 1.1.1",
+        "summary": "Stock Gingerbread build.",
+        "device": "inc",
+        "product": "OMGB",
+        "incremental": "2",
+        "url": "r2doesinc.bitsurge.net/OMGB_inc-1.1.1.zip",
+        "addons": 
         [
-        {
-              name: "Google Apps",
-              urls:
-                   [
-                   "http://www.r2doesinc.bitsurge.net/GAPPS.zip"
-                   ]
-        }
+            {
+            "name": "Google Apps",
+            "url": "http://www.r2doesinc.bitsurge.net/GAPPS.zip"
+            }
         ]
-},
-
-{
-        name: "OMGB 1.1.1",
-        summary: "Stock Gingerbread build.",
-        device: "supersonic",
-        product: "OMGB",
-        incremental: 2,
-        url: "http://www.r2doesinc.bitsurge.net/OMGB_supersonic-1.1.1.zip",
-        addons:
+    },
+    {
+        "name": "OMGB 1.1.1",
+        "summary": "Stock Gingerbread build.",
+        "device": "supersonic",
+        "product": "OMGB",
+        "incremental": "2",
+        "url": "r2doesinc.bitsurge.net/OMGB_supersonic-1.1.1.zip",
+        "addons": 
         [
-        {
-              name: "Google Apps",
-              urls:
-                   [
-                   "http://www.r2doesinc.bitsurge.net/GAPPS.zip"
-                   ]
-        }
+            {
+            "name": "Google Apps",
+            "url": "http://www.r2doesinc.bitsurge.net/GAPPS.zip"
+            }
         ]
-},
-
-{
-        name: "OMGB 1.1.0",
-        summary: "Stock Gingerbread build.",
-        device: "supersonic",
-        product: "OMGB",
-        incremental: 1,
-        url: "http://www.r2doesinc.bitsurge.net/OMGB_supersonic-1.1.0.zip",
-        addons:
+    },
+    {
+        "name": "OMGB 1.1.0",
+        "summary": "Stock Gingerbread build.",
+        "device": "supersonic",
+        "product": "OMGB",
+        "incremental": "1",
+        "url": "r2doesinc.bitsurge.net/OMGB_supersonic-1.1.0.zip",
+        "addons": 
         [
-        {
-              name: "Google Apps",
-              urls:
-                   [
-                   "http://www.r2doesinc.bitsurge.net/GAPPS.zip"
-                   ]
-        }
+            {
+            "name": "Google Apps",
+            "url": "http://www.r2doesinc.bitsurge.net/GAPPS.zip"
+            }
         ]
-},
-
-{
-        name: "OMGB 1.1.0",
-        summary: "Stock Gingerbread build.",
-        device: "inc",
-        product: "OMGB",
-        incremental: 1,
-        url: "http://www.r2doesinc.bitsurge.net/OMGB_inc-1.1.0.zip",
-        addons:
+    },
+    {
+        "name": "OMGB 1.1.0",
+        "summary": "Stock Gingerbread build.",
+        "device": "inc",
+        "product": "OMGB",
+        "incremental": "1",
+        "url": "r2doesinc.bitsurge.net/OMGB_inc-1.1.0.zip",
+        "addons": 
         [
-        {
-              name: "Google Apps",
-              urls:
-                   [
-                   "http://www.r2doesinc.bitsurge.net/GAPPS.zip"
-                   ]
-        }
+            {
+            "name": "Google Apps",
+            "url": "http://www.r2doesinc.bitsurge.net/GAPPS.zip"
+            }
         ]
-},
-
-{
-        name: "OMFGB 1.1.0",
-        summary: "Custom Gingerbread build.",
-        device: "supersonic",
-        product: "OMFGB",
-        incremental: 3,
-        url: "http://r2doesinc.bitsurge.net/OMFGB-1.1.0_evo.zip",
-        addons:
-       [
-       {
-            name: "Google Apps",
-            urls:
-                 [
-                 "http://www.r2doesinc.bitsurge.net/GAPPS.zip"
-                  ]
-       }
-       ]
-},
-    
-{
-        name: "OMFGB 1.1.0",
-        summary: "Custom Gingerbread build.",
-        device: "inc",
-        product: "OMFGB",
-        incremental: 3,
-        url: "http://r2doesinc.bitsurge.net/OMFGB-1.1.0_inc.zip",
-        addons:
+    },
+    {
+        "name": "OMFGB 1.1.0",
+        "summary": "Custom Gingerbread build.",
+        "device": "supersonic",
+        "product": "OMFGB",
+        "incremental": "3",
+        "url": "http://r2doesinc.bitsurge.net/OMFGB-1.1.0_evo.zip",
+        "addons": 
         [
-        {
-             name: "Google Apps",
-             urls:
-                  [
-                  "http://www.r2doesinc.bitsurge.net/GAPPS.zip"
-                  ]
-        }
+            {
+            "name": "Google Apps",
+            "url": "http://www.r2doesinc.bitsurge.net/GAPPS.zip"
+            }
         ]
-},
-
-{
-        name: "OMFGB 1.1.1",
-        summary: "Custom Gingerbread build.",
-        device: "supersonic",
-        product: "OMFGB",
-        incremental: 4,
-        url: "http://droid-themes.com/Downloads/OMFGB/OMFGB_supersonic-1.1.1.zip",
-        addons:
+    },
+    {
+        "name": "OMFGB 1.1.0",
+        "summary": "Custom Gingerbread build.",
+        "device": "inc",
+        "product": "OMFGB",
+        "incremental": "3",
+        "url": "http://r2doesinc.bitsurge.net/OMFGB-1.1.0_inc.zip",
+        "addons": 
         [
-        {
-             name: "Google Apps",
-             urls:
-                  [
-                  "http://www.r2doesinc.bitsurge.net/GAPPS.zip"
-                  ]
-        }
+            {
+            "name": "Google Apps",
+            "url": "http://www.r2doesinc.bitsurge.net/GAPPS.zip"
+            }
         ]
-},
-
-{
-        name: "OMFGB 1.1.1",
-        summary: "Custom Gingerbread build.",
-        device: "inc",
-        product: "OMFGB",
-        incremental: 4,
-        url: "http://droid-themes.com/Downloads/OMFGB/OMFGB_inc-1.1.1.zip",
-        addons:
+    },
+    {
+        "name": "OMFGB 1.1.1",
+        "summary": "Custom Gingerbread build.",
+        "device": "supersonic",
+        "product": "OMFGB",
+        "incremental": "4",
+        "url": "http://droid-themes.com/Downloads/OMFGB/OMFGB_supersonic-1.1.1.zip",
+        "addons": 
         [
-        {
-              name: "Google Apps",
-              urls:
-                   [
-                   "http://www.r2doesinc.bitsurge.net/GAPPS.zip"
-                   ]
-        }
+            {
+            "name": "Google Apps",
+            "url": "http://www.r2doesinc.bitsurge.net/GAPPS.zip"
+            }
         ]
-}
-]
+    },
+    {
+        "name": "OMFGB 1.1.1",
+        "summary": "Custom Gingerbread build.",
+        "device": "inc",
+        "product": "OMFGB",
+        "incremental": "4",
+        "url": "http://droid-themes.com/Downloads/OMFGB/OMFGB_inc-1.1.1.zip",
+        "addons": 
+        [
+            {
+            "name": "Google Apps",
+            "url": "http://www.r2doesinc.bitsurge.net/GAPPS.zip"
+            }
+        ]
+    }
+	]
 }
